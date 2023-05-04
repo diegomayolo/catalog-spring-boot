001062991c74c5b64f4c6720e9d92ed0901ef7e9
Diego Luis Mayolo <diego.mayolo@outlook.com>
2023-05-04 19:28:14 -0300

    H2 security configuration

--- backend/src/main/java/com/dm/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/dm/dscatalog/config/ResourceServerConfig.java
@@ -2,12 +2,15 @@ package com.dm.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -16,12 +19,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter
     private static final String ROLE_OPERATOR = "OPERATOR";
     private static final String ROLE_ADMIN    = "ADMIN";
 
-
-    private static final String[] PUBLIC = { "/oauth/token" };
-
+    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+    
     private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
 
     private static final String[] ADMIN = { "/users/**" };
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     private JwtTokenStore tokenStore;
@@ -47,6 +52,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter
     @Override
     public void configure( HttpSecurity http ) throws Exception
     {
+        // liberar h2 para testes de desenvolvimento
+        if ( Arrays.asList( env.getActiveProfiles() ).contains( "test" ) )
+        {
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests().antMatchers( PUBLIC ).permitAll()
                                 .antMatchers( HttpMethod.GET, OPERATOR_OR_ADMIN ).permitAll()
                                 .antMatchers(  OPERATOR_OR_ADMIN ).hasAnyRole( ROLE_OPERATOR, ROLE_ADMIN )
