78784915b977e4836065d914eb085b31ea828cd2
Diego Luis Mayolo <diego.mayolo@outlook.com>
2023-05-04 19:02:17 -0300

    Token additional informations and externalized environment variables

--- backend/src/main/java/com/dm/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/dm/dscatalog/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.dm.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +14,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig
 {
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder()
     {
@@ -23,7 +27,7 @@ public class AppConfig
     public JwtAccessTokenConverter accessTokenConverter()
     {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("MY-JWT-SECRET");
+        tokenConverter.setSigningKey( jwtSecret );
         return tokenConverter;
     }
 
